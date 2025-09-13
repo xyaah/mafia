27184fc77e1b013375e433bb0bcfd3cdd206ca21
xya <xyaah>
2025-09-13 20:45:45 +0300

    connect to ws and send message

---
+++ game.js
@@ -0,0 +1,16 @@
+const isDevelopment =
+  location.hostname === "localhost" || location.hostname === "127.0.0.1";
+
+const ws = new WebSocket(
+  isDevelopment
+    ? "ws://localhost:8787/mafia"
+    : "wss://smolgames.xyaah.workers.dev"
+);
+
+ws.addEventListener("message", (e) => {
+  console.log(e.data);
+});
+
+ws.addEventListener("open", () => {
+  ws.send("hello from client");
+});
