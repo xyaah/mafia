dadc066e571d8748b6fc2d93764dc0f58ff0c3b1
xya <xyaah>
2025-09-15 02:47:28 +0300

    mafia members can now vote on somebody to kill

--- game.js
+++ game.js
@@ -28,6 +28,13 @@ function parseMessage(message) {
 }
 
 let lobbyOthers = [];
+let others = [];
+let otherMafias = [];
+let myRole;
+
+function print(text) {
+  story.innerText += "\n> " + text;
+}
 
 function isLobbyOwner() {
   const me = lobbyOthers.find((other) => other.me);
@@ -58,12 +65,11 @@ const story = document.querySelector("#game-story");
 const timer = document.querySelector("#timer");
 
 function gameStarted() {
+  others = [...lobbyOthers];
+  lobbyOthers = [];
   document.querySelector("#lobby").style.display = "none";
-  story.innerText = `> you enter a strange new land, surrounded by unknown people.`;
-  setTimeout(
-    () => (story.innerText += "\n> your identity is revealed.. "),
-    4000
-  );
+  print(`you enter a strange new land, surrounded by unknown people.`);
+  setTimeout(() => print("your identity is revealed.. "), 4000);
 }
 
 function roleName(role) {
@@ -75,34 +81,99 @@ function roleName(role) {
 }
 
 function roleRevealed(role) {
+  myRole = role;
   story.innerText += roleName(role);
 }
 
 function startTime(at, ms) {
-  let interval;
-  interval = setInterval(() => {
-    const timePassed = Date.now() - at;
-    const timeRem = Math.max(0, ms - timePassed) / 1000;
-    if (timeRem === 0) clearInterval(interval);
-    else {
-      const min = Math.floor(timeRem / 60);
-      const sec = Math.floor(timeRem % 60);
-      timer.innerText = `${min.toString().padStart(2, "0")}:${sec
-        .toString()
-        .padStart(2, "0")}`;
-    }
-  }, 30);
+  return new Promise((res) => {
+    let interval;
+    interval = setInterval(() => {
+      const timePassed = Date.now() - at;
+      const timeRem = Math.max(0, ms - timePassed) / 1000;
+      if (timeRem === 0) {
+        clearInterval(interval);
+        res();
+      } else {
+        const min = Math.floor(timeRem / 60);
+        const sec = Math.floor(timeRem % 60);
+        timer.innerText = `${min.toString().padStart(2, "0")}:${sec
+          .toString()
+          .padStart(2, "0")}`;
+      }
+    }, 30);
+  });
 }
 
 function nightTime() {
-  story.innerText +=
-    "\n> night falls over the town. you are immersed in darkness..";
+  print("night falls over the town. you are immersed in darkness..");
 }
 
+/** @returns {Promise<number|undefined>} */
+function askSelectPlayer(timeout) {
+  return new Promise((res) => {
+    let resolved = false;
+    /** @type {HTMLUListElement} */
+    const selector = document.querySelector("#player-selector");
+    while (selector.children.length) selector.removeChild(selector.children[0]);
+    function hide() {
+      selector.style.display = "none";
+    }
+    for (const other of others) {
+      if (otherMafias.includes(other.id)) continue;
+      const el = document.createElement("li");
+      const btn = document.createElement("button");
+      btn.innerText = other.name;
+      btn.addEventListener("click", () => {
+        if (!resolved) {
+          resolved = true;
+          res(other.id);
+          hide();
+        }
+      });
+      el.appendChild(btn);
+      selector.appendChild(el);
+    }
+    selector.style.display = "unset";
+    setTimeout(() => {
+      if (!resolved) {
+        resolved = true;
+        res(undefined);
+        hide();
+      }
+    }, timeout);
+  });
+}
+
+function showChat(show) {}
+
 function mafiaAskVote() {
-  story.innerText =
-    "\n> the mafia gathers secretly to decide on some to kill...";
-  startTime(Date.now(), 3 * 60 * 1000);
+  const timeAlloted = 3 * 60 * 1000;
+  print("the mafia gathers secretly to decide on someone to kill...");
+  showChat(true);
+  startTime(Date.now(), timeAlloted).then(() => showChat(false));
+  askSelectPlayer(timeAlloted).then((player) => {
+    if (player) {
+      ws.send(`mafia_vote ${player}`);
+    }
+  });
+}
+
+function onDied(causeOfDeath) {
+  switch (causeOfDeath) {
+    case "mafia_kill":
+      print("the mafia has killed you. this is it for you");
+      break;
+
+    case "town_vote":
+      print(
+        "the town has decided to banish you. this is where your story ends"
+      );
+      break;
+
+    default:
+      break;
+  }
 }
 
 ws.addEventListener("message", (e) => {
@@ -143,6 +214,15 @@ ws.addEventListener("message", (e) => {
     }
     case "mafia_ask_vote": {
       mafiaAskVote();
+      break;
+    }
+    case "other_mafias": {
+      otherMafias = JSON.parse(args.join(" "));
+      break;
+    }
+    case "died": {
+      const causeOfDeath = args[0];
+      onDied(causeOfDeath);
       break;
     }
     default:
