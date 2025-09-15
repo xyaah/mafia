504714d5ed9677989b4ddcfb07b3f69a8cfe5804
xya <xyaah>
2025-09-15 06:47:47 +0300

    hide game UI during lobby

--- game.js
+++ game.js
@@ -76,6 +76,7 @@ function gameStarted() {
   others = [...lobbyOthers];
   lobbyOthers = [];
   document.querySelector("#lobby").style.display = "none";
+  document.querySelector("#game-container").style.display = "none";
   print(`you enter a strange new land, surrounded by unknown people.`);
   setTimeout(() => print("your identity is revealed.. "), 4000);
 }
